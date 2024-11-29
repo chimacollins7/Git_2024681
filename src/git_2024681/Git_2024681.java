aedfc7a7427d3a128735ecd4599d823d9c2acdce
chimacollins7 <akonuchima7@gmail.com>
2024-11-29 11:02:52 +0000

    added a simple print

--- src/git_2024681/Git_2024681.java
+++ src/git_2024681/Git_2024681.java
@@ -16,6 +16,7 @@ public class Git_2024681 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("chimacollins-2024681");
     }
     
 }
